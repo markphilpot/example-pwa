5eee9bd6e0a7ab252c5655fc0150f20a1c60de13
Mark Philpot <mark.philpot@gmail.com>
2024-10-11 13:35:07 -0700

    Make a tweak

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Testing ability to update AGAIN
+          Testing ability to update AGAIN AGAIN
         </p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
